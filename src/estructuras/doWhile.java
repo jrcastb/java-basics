413806a1624a27092eb0038b5fdef98b54cedd31
Jose Castillo <jrandolini@gmail.com>
2023-03-08 19:35:43 -0500

    do While added

---
+++ src/estructuras/doWhile.java
@@ -0,0 +1,38 @@
+package estructuras;
+
+import java.util.Scanner;
+
+public class doWhile {
+
+    public static void main(String[] args) {
+        byte opt = 0;
+        Scanner scanner = new Scanner(System.in);
+        do {
+            // Se ubican instrucciones que se ejecutan por lo menos una vez
+            System.out.println("1. Ejecutar la opcion 1");
+            System.out.println("2. Ejecutar la opcion 2");
+            System.out.println("3. Ejecutar la opcion 3");
+            System.out.println("4. Ejecutar la opcion 4");
+
+            System.out.println("Escriba un numero para seleccionar la opcion que desea ejecutar: ");
+
+            opt = scanner.nextByte();
+
+        } while (opt <1 || opt > 4);
+
+        switch (opt){
+            case 1:
+                System.out.println("Se ejecutó la opción #1");
+                break;
+            case 2:
+                System.out.println("Se ejecutó la opción #2");
+                break;
+            case 3:
+                System.out.println("Se ejecutó la opción #3");
+                break;
+            case 4:
+                System.out.println("Se ejecutó la opción #4");
+                break;
+        }
+    }
+}
